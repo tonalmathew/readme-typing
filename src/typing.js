7e0cfb3ef066bfb0a181be0e965f83af6bf688b6
Tonal Mathew <tonalmathew.dev@gmail.com>
2023-08-14 23:17:46 +0530

    chore: use octokit

--- src/typing.js
+++ src/typing.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 export async function run() {
   try {
-    const token = core.getInput('GH_TOKEN');
+    const token = core.getInput('token');
     const inputText = core.getInput('input-text');
     const textArray = inputText.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 
